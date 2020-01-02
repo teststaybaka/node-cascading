7daf7d4db973e806b92ab9f66e377495fee65fb9
Yuxuan Xie <teststaybaka@gmail.com>
2020-01-01 22:42:19 -0800

    Use X-SESSION as custom header

--- common.ts
+++ common.ts
@@ -4,6 +4,7 @@ export let CONTENT_TYPE_TEXT = 'text/plain';
 export let CONTENT_TYPE_HTML = 'text/html';
 export let CONTENT_TYPE_JSON = 'application/json';
 export let CONTENT_TYPE_BINARY_STREAM = 'application/octet-stream';
+export let SESSION_HEADER = 'X-SESSION';
 
 export function findWithDefault<Key, Value>(map: Map<Key, Value>, key: Key, defaultValue: Value): Value {
   let value = map.get(key);
