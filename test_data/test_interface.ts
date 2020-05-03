1506727e8f4d00720a08d4bdace9086740f06b0e
teststaybaka <teststaybaka@gmail.com>
2020-05-03 05:41:21 +0000

    Add test_interface/_another.ts and remove MessageDefinition

---
+++ test_data/test_interface.ts
@@ -0,0 +1,12 @@
+export interface TestObject {
+  field1: string,
+  field2: string,
+  field3: boolean,
+}
+
+export enum TestColor {
+  RED = 1,
+  GREEN = 3,
+  BLUE = 10,
+}
+
---
+++ test_data/test_interface_another.ts
@@ -0,0 +1,8 @@
+import { TestObject, TestColor } from './test_interface'
+
+export interface TestNestedObject {
+  nestedField1: TestObject,
+  color: TestColor,
+  color2: TestColor,
+}
+
