e59eb097afc74211130f092ded128893d72f862f
Yuxuan Xie <teststaybaka@gmail.com>
2020-06-12 07:44:00 +0000

    Add readBuffer and readString to receive readable stream.

--- stream_reader.ts
+++ stream_reader.ts
@@ -4,31 +4,41 @@ import { newInternalError } from "./errors";
 export class StreamReader {
   private static BUFFER_LIMIT = 50 * 1024 * 1024;
 
-  public async readJson(incoming: stream.Readable): Promise<any> {
-    let buffer = await new Promise<number[]>((resolve, reject): void => {
-      let buffer: number[] = [];
+  public async readBuffer(incoming: stream.Readable): Promise<Buffer> {
+    let buffers = await new Promise<Buffer[]>((resolve, reject): void => {
+      let buffers: Buffer[] = [];
+      let size = 0;
       incoming.on("data", (chunk): void => {
-        if (buffer.length + chunk.length > StreamReader.BUFFER_LIMIT) {
-          reject(
-            newInternalError(
+        if (size + chunk.length> StreamReader.BUFFER_LIMIT) {
+          let error = newInternalError(
               `Stream data exceeds buffer limit, ${StreamReader.BUFFER_LIMIT}.`
             )
+          reject(error
           );
+          incoming.destroy(error);
           return;
         }
 
-        for (let i = 0, len = chunk.length; i < len; i++) {
-          buffer.push(chunk[i] as number);
-        }
+        buffers.push(chunk);
+        size += chunk.length;
       });
       incoming.on("error", (err): void => {
         reject(newInternalError("Stream encountered an error!", err));
       });
       incoming.on("end", (): void => {
-        resolve(buffer);
+        resolve(buffers);
       });
     });
-    let jsonString = Buffer.from(buffer).toString();
-    return JSON.parse(jsonString);
+    return Buffer.concat(buffers);
+  }
+
+  public async readString(incoming: stream.Readable): Promise<any> {
+    let buffer = await this.readBuffer(incoming);
+    return buffer.toString();
+  }
+
+  public async readJson(incoming: stream.Readable): Promise<any> {
+    let str = await this.readString(incoming);
+    return JSON.parse(str);
   }
 }
