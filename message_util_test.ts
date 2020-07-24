a081d04191b83364a0a1d00fd4b0ebf29480bf38
Yuxuan Xie <teststaybaka@gmail.com>
2020-07-24 23:55:34 +0000

    Cover array in tests

--- message_util_test.ts
+++ message_util_test.ts
@@ -11,10 +11,7 @@ function testParseEnum(input: string | number, expected: number) {
   let colorEnumDescriptor: EnumDescriptorUntyped = {
     name: "Color",
     enumValues: [
-      {
-        name: "RED",
-        value: 10,
-      },
+      { name: "RED", value: 10 },
       { name: "BLUE", value: 1 },
       { name: "GREEN", value: 2 },
     ],
@@ -65,14 +62,13 @@ function testParsingMessageWithPrimitiveTypes(raw: any) {
     name: "User",
     fields: [
       { name: "id", type: MessageFieldType.NUMBER },
-      {
-        name: "isPaid",
-        type: MessageFieldType.BOOLEAN,
-      },
-      {
-        name: "nickname",
-        type: MessageFieldType.STRING,
-      },
+      { name: "isPaid", type: MessageFieldType.BOOLEAN },
+      { name: "nickname", type: MessageFieldType.STRING },
+      { name: "email", type: MessageFieldType.STRING },
+      { name: "idHistory", type: MessageFieldType.NUMBER, isArray: true },
+      { name: "isPaidHistory", type: MessageFieldType.BOOLEAN, isArray: true },
+      { name: "nicknameHistory", type: MessageFieldType.STRING, isArray: true },
+      { name: "emailHistory", type: MessageFieldType.STRING, isArray: true },
     ],
   };
 
@@ -91,30 +87,64 @@ class ParseMessagePrimtivesAllPopulated implements TestCase {
       id: 12,
       isPaid: true,
       nickname: "jack",
+      email: "test@gmail.com",
+      idHistory: [11, 20, 855],
+      isPaidHistory: [false, true, false, false],
+      nicknameHistory: ["queen", "king", "ace"],
+      emailHistory: ["test1@test.com", "123@ttt.com"],
     });
 
     // Verify
     assert(parsed.id === 12);
-    assert(parsed.isPaid);
+    assert(parsed.isPaid === true);
     assert(parsed.nickname === "jack");
+    assert(parsed.email === "test@gmail.com");
+    assert(parsed.idHistory.length === 3);
+    assert(parsed.idHistory[0] === 11);
+    assert(parsed.idHistory[1] === 20);
+    assert(parsed.idHistory[2] === 855);
+    assert(parsed.isPaidHistory.length === 4);
+    assert(parsed.isPaidHistory[0] === false);
+    assert(parsed.isPaidHistory[1] === true);
+    assert(parsed.isPaidHistory[2] === false);
+    assert(parsed.isPaidHistory[3] === false);
+    assert(parsed.nicknameHistory.length === 3);
+    assert(parsed.nicknameHistory[0] === "queen");
+    assert(parsed.nicknameHistory[1] === "king");
+    assert(parsed.nicknameHistory[2] === "ace");
+    assert(parsed.emailHistory.length === 2);
+    assert(parsed.emailHistory[0] === "test1@test.com");
+    assert(parsed.emailHistory[1] === "123@ttt.com");
   }
 }
 
-class ParseMessagePrimtivesOverlapping implements TestCase {
-  public name = "ParseMessagePrimtivesOverlapping";
+class ParseMessagePrimtivesSkipUnmatched implements TestCase {
+  public name = "ParseMessagePrimtivesSkipUnmatched";
 
   public execute() {
     let parsed = testParsingMessageWithPrimitiveTypes({
-      id: 12,
+      id: "12",
       isPaid: "true",
+      email: "test@gmail.com",
       random: "jack",
+      idHistory: [11, "20", {}],
+      nicknameHistory: [],
+      emailHistory: ["test1@test.com", "123@ttt.com"],
     });
 
     // Verify
-    assert(parsed.id === 12);
+    assert(parsed.id === undefined);
     assert(parsed.isPaid === undefined);
     assert(parsed.nickname === undefined);
+    assert(parsed.email === "test@gmail.com");
     assert(parsed.random === undefined);
+    assert(parsed.idHistory.length === 1);
+    assert(parsed.idHistory[0] === 11);
+    assert(parsed.isPaidHistory.length === 0);
+    assert(parsed.nicknameHistory.length === 0);
+    assert(parsed.emailHistory.length === 2);
+    assert(parsed.emailHistory[0] === "test1@test.com");
+    assert(parsed.emailHistory[1] === "123@ttt.com");
   }
 }
 
@@ -123,17 +153,10 @@ function testParsingNestedMessages(raw: any) {
   let colorEnumDescriptor: EnumDescriptorUntyped = {
     name: "Color",
     enumValues: [
-      {
-        name: "RED",
-        value: 10,
-      },
+      { name: "RED", value: 10 },
       { name: "BLUE", value: 1 },
       { name: "GREEN", value: 2 },
     ],
-  };
-  let paymentInfoMessageDescriptor: MessageDescriptorUntyped = {
-    name: "PaymentInfo",
-    fields: [{ name: "added", type: MessageFieldType.BOOLEAN }],
   };
   let userInfoMessaeDescriptor: MessageDescriptorUntyped = {
     name: "UserInfo",
@@ -148,25 +171,36 @@ function testParsingNestedMessages(raw: any) {
         enumDescriptor: colorEnumDescriptor,
       },
       {
-        name: "paymentInfo",
-        type: MessageFieldType.MESSAGE,
-        messageDescriptor: paymentInfoMessageDescriptor,
+        name: "preferredColor",
+        type: MessageFieldType.ENUM,
+        enumDescriptor: colorEnumDescriptor,
+      },
+      {
+        name: "colorHistory",
+        type: MessageFieldType.ENUM,
+        enumDescriptor: colorEnumDescriptor,
+        isArray: true,
       },
     ],
+  };
+  let creditCardMessageDescriptor: MessageDescriptorUntyped = {
+    name: "CreditCard",
+    fields: [{ name: "cardNumber", type: MessageFieldType.NUMBER }],
   };
   let userMessageDescriptor: MessageDescriptorUntyped = {
     name: "User",
     fields: [
       { name: "id", type: MessageFieldType.NUMBER },
       {
-        name: "detailedInfo",
+        name: "userInfo",
         type: MessageFieldType.MESSAGE,
         messageDescriptor: userInfoMessaeDescriptor,
       },
       {
-        name: "preferColor",
-        type: MessageFieldType.ENUM,
-        enumDescriptor: colorEnumDescriptor,
+        name: "creditCards",
+        type: MessageFieldType.MESSAGE,
+        messageDescriptor: creditCardMessageDescriptor,
+        isArray: true,
       },
     ],
   };
@@ -183,47 +217,57 @@ class ParseMessageNestedAllPopulated implements TestCase {
 
   public execute() {
     let parsed = testParsingNestedMessages({
-      id: 29,
-      detailedInfo: {
+      id: 25,
+      userInfo: {
         intro: "student",
-        paymentInfo: {
-          added: true,
-        },
         backgroundColor: "RED",
+        preferredColor: 1,
+        colorHistory: ["BLUE", "GREEN", 10],
       },
-      preferColor: "BLUE",
+      creditCards: [{ cardNumber: 1010 }, { cardNumber: 2020 }],
     });
 
     // Verify
-    assert(parsed.id === 29);
-    assert(parsed.detailedInfo.intro === "student");
-    assert(parsed.detailedInfo.paymentInfo.added);
-    assert(parsed.detailedInfo.backgroundColor === 10);
-    assert(parsed.preferColor === 1);
+    assert(parsed.id === 25);
+    assert(parsed.userInfo.intro === "student");
+    assert(parsed.userInfo.backgroundColor === 10);
+    assert(parsed.userInfo.preferredColor === 1);
+    assert(parsed.userInfo.colorHistory.length === 3);
+    assert(parsed.userInfo.colorHistory[0] === 1);
+    assert(parsed.userInfo.colorHistory[1] === 2);
+    assert(parsed.userInfo.colorHistory[2] === 10);
+    assert(parsed.creditCards.length === 2);
+    assert(parsed.creditCards[0].cardNumber === 1010);
+    assert(parsed.creditCards[1].cardNumber === 2020);
   }
 }
 
-class ParseMessageNestedOverlapping implements TestCase {
-  public name = "ParseMessageNestedOverlapping";
+class ParseMessageNestedSkipUnmatched implements TestCase {
+  public name = "ParseMessageNestedSkipUnmatched";
 
   public execute() {
     let parsed = testParsingNestedMessages({
-      id: 29,
-      detailedInfo: {
-        intro: false,
-        over: "overly",
-        backgroundColor: "RED",
+      id: 25,
+      userInfo: {
+        intro: "student",
+        backgroundColor: false,
+        preferredColor: 1,
+        colorHistory: [true, "BLUE"],
       },
-      preferColor: 123,
+      creditCards: [{ cardNumber: "1010" }, 2020, {}, { cardNumber: 3030 }],
     });
 
     // Verify
-    assert(parsed.id === 29);
-    assert(parsed.detailedInfo.intro === undefined);
-    assert(parsed.detailedInfo.paymentInfo === undefined);
-    assert(parsed.detailedInfo.over === undefined);
-    assert(parsed.detailedInfo.backgroundColor === 10);
-    assert(parsed.preferColor === undefined);
+    assert(parsed.id === 25);
+    assert(parsed.userInfo.intro === "student");
+    assert(parsed.userInfo.backgroundColor === undefined);
+    assert(parsed.userInfo.preferredColor === 1);
+    assert(parsed.userInfo.colorHistory.length === 1);
+    assert(parsed.userInfo.colorHistory[0] === 1);
+    assert(parsed.creditCards.length === 3);
+    assert(parsed.creditCards[0].cardNumber === undefined);
+    assert(parsed.creditCards[1].cardNumber === undefined);
+    assert(parsed.creditCards[2].cardNumber === 3030);
   }
 }
 
@@ -233,7 +277,7 @@ runTests("MessageUtilTest", [
   new ParseEnumValueFromString(),
   new ParseEnumValueFromNonexistingString(),
   new ParseMessagePrimtivesAllPopulated(),
-  new ParseMessagePrimtivesOverlapping(),
+  new ParseMessagePrimtivesSkipUnmatched(),
   new ParseMessageNestedAllPopulated(),
-  new ParseMessageNestedOverlapping(),
+  new ParseMessageNestedSkipUnmatched(),
 ]);
