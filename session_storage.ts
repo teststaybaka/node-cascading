cf97dc890488b013ca4e6c46134c5203ca2dc98d
Yuxuan Xie <teststaybaka@gmail.com>
2020-08-22 18:19:09 +0000

    Migrate SessionStorage

---
+++ session_storage.ts
@@ -0,0 +1,17 @@
+export class SessionStorage {
+  private static NAME = 'session';
+
+  public save(session: string): void {
+    localStorage.setItem(SessionStorage.NAME, session);
+  }
+
+  public read(): string|undefined {
+    return localStorage.getItem(SessionStorage.NAME);
+  }
+
+  public clear(): void {
+    localStorage.removeItem(SessionStorage.NAME);
+  }
+}
+
+export let SESSION_STORAGE = new SessionStorage();
