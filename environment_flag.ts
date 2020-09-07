add57f85ced4ed641d423a98991c1d54232e3398
Yuxuan Xie <teststaybaka@gmail.com>
2020-09-07 22:02:10 +0000

    Rename to getEnvironmentFlag

--- environment_flag.ts
+++ environment_flag.ts
@@ -3,6 +3,6 @@ import { parseJsonString } from "./named_type_util";
 
 declare let environment_flag: string;
 
-export function getEnvironment(): Environment {
+export function getEnvironmentFlag(): Environment {
   return parseJsonString(environment_flag, ENVIRONMENT);
 }
